66594494e06e92de3eda6f0f229903f540c01c32
sanketpatel <patelsanketr98@gmail.com>
2025-03-22 13:12:46 -0400

    fix: standardize quotes and add test route for user endpoints in server.js

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import trackingRoutes from './routes/trackingRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import trackingRoutes from "./routes/trackingRoutes.js";
 import cors from "cors";
-import userRoutes from './routes/userRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config(); // Load environment variables from .env file
 console.log("MongoDB URI:", process.env.MONGODB_URI); // Debugging
 
-
 const app = express();
 app.use(cors());
-app.set('trust proxy', true)
+app.set("trust proxy", true);
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Parse incoming JSON requests
 
 app.get("/", (req, res) => {
-    res.send("Hello from Express on Vercel!");
-  });
+  res.send("Hello from Express on Vercel!");
+});
 
 // Use tracking routes
-app.use('/api', trackingRoutes);
+app.use("/api", trackingRoutes);
 
+app.get("/user/test", (req, res) => {
+  res.send("User route works!");
+});
 // Use User routes
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
